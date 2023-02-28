0a2d5fcd9c5c701d24e75ac44e482f640a8215bd
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2023-02-28 08:31:22 -0600

    item writer works

--- src/main/java/cnlabs/CustomerWebsite/Config/BatchConfiguration.java
+++ src/main/java/cnlabs/CustomerWebsite/Config/BatchConfiguration.java
@@ -4,21 +4,30 @@ import cnlabs.CustomerWebsite.Models.Customer;
 import cnlabs.CustomerWebsite.Repos.CustomerRepository;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.annotation.AfterStep;
+import org.springframework.batch.core.annotation.BeforeStep;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.data.RepositoryItemReader;
 import org.springframework.batch.item.data.builder.RepositoryItemReaderBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
+import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
 
@@ -40,9 +49,9 @@ public class BatchConfiguration {
     @Bean
     public Step step1(StepBuilderFactory stepBuilderFactory, ItemReader<Customer> repositoryReader, NameProcessor processor, CustomerWriter writer) {
         return stepBuilderFactory.get("step-1")
-                .<Customer, Customer>chunk(100)
+                .<Customer, Customer>chunk(1)
                 .reader(repositoryReader)      // EXTRACT
-                .processor(processor)   // TRANSFORM
+//                .processor(processor)   // TRANSFORM
                 .writer(writer)         // LOAD
                 .build();
     }
@@ -69,17 +78,50 @@ public class BatchConfiguration {
         }
     }
 
-    // ItemWriter
     @Component
     public static class CustomerWriter implements ItemWriter<Customer> {
 
-        @Autowired
-        private CustomerRepository customerRepository;
+        private final Resource outputResource = new FileSystemResource("src/main/resources/outputData-" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm-ss")) + ".csv");
+        private FlatFileItemWriter<Customer> writer;
+
+        @BeforeStep
+        public void beforeStep() throws Exception {
+            writer = new FlatFileItemWriter<>();
+
+            // Set output file location
+            writer.setResource(outputResource);
+
+            // All job repetitions should "append" to same output file
+            writer.setAppendAllowed(true);
+
+            // Name field values sequence based on object properties
+            writer.setLineAggregator(new DelimitedLineAggregator<Customer>() {
+                {
+                    setDelimiter(",");
+                    setFieldExtractor(new BeanWrapperFieldExtractor<Customer>() {
+                        {
+                            setNames(new String[]{"id", "fullName", "emailAddress", "age", "address", "book"});
+                        }
+                    });
+                }
+            });
+
+            // Open the writer
+            writer.open(new ExecutionContext());
+        }
 
         @Override
-        public void write(List<? extends Customer> employees) throws InterruptedException {
-            customerRepository.saveAll(employees);
-            System.out.println("Saved employees: " + employees);
+        public void write(List<? extends Customer> customers) throws Exception {
+            writer.write(customers);
+            System.out.println("Saved customers: " + customers);
+        }
+
+        @AfterStep
+        public void afterStep() throws Exception {
+            // Close the writer
+            writer.close();
         }
     }
+
+
 }
